b58a53e1731ac4b539268e12b334e738eeb2127a
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-03-19 15:34:33 -0500

    style: better style for workers list

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
 
-// load querys
 require_once 'database/query.php';
 $r_asistencia = new Registro_Asistencia();
 
 echo "<h1>Trabajadores</h1>";
-foreach($r_asistencia->get_all() as $row){
-    foreach($row as $item){
-        if($item == $row['imagen_rostro']){
-            echo "<img src='data:image/jpeg;base64,".base64_encode($item)."'/>";
-        }else{
-            echo $item."<br>";
+echo "<table border='1' style='width:100%; border-collapse: collapse; text-align: center;'>";
+echo "<tr><th>ID</th><th>Nombre</th><th>Imagen</th></tr>";
+
+foreach ($r_asistencia->get_all() as $row) {
+    echo "<tr>";
+    
+    foreach ($row as $key => $item) {
+        if ($key == 'imagen_rostro') {
+            echo "<td><img src='data:image/jpeg;base64," . base64_encode($item) . "' style='width: 50px; height: 50px;'/></td>";
+        } else {
+            echo "<td>" . htmlspecialchars($item) . "</td>";
         }
     }
+    
+    echo "</tr>";
 }
+echo "</table>";
+
+
 
 ?>
