0e70fb951cfda458edd1099fd44bf1067f215e45
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-03-19 16:02:41 -0500

    feat: get all asisttance from database

--- database/query.php
+++ database/query.php
@@ -2,30 +2,53 @@
 
 require_once 'connect.php';
 
-class Registro_Asistencia{
-
-    private $conn;
+class main{
+    public $conn;
 
     public function __construct(){
         $database = new database();
         $this->conn = $database->conn;
     }
+}
+
+class Empleados extends main{
+    public $conn;
 
     public function get_all(){
-        $sql = "SELECT Empleados.cedula, Empleados.nombres, Empleados.apellidos, Empleados.email, Cargos.nombre as cargo, Departamentos.nombre as departamento, DatosBiometricos.imagen_rostro from Empleados 
+        $sql = "SELECT 
+            Empleados.cedula, Empleados.nombres, 
+            Empleados.apellidos, Empleados.email, 
+            Cargos.nombre as cargo, 
+            Departamentos.nombre as departamento, 
+            DatosBiometricos.imagen_rostro 
+        from Empleados 
         INNER JOIN Departamentos ON Departamentos.id_departamento = Empleados.id_departamento 
         INNER JOIN Cargos on Cargos.id_cargo = Empleados.id_cargo 
         INNER JOIN DatosBiometricos ON DatosBiometricos.id_empleado = Empleados.id_empleado; ";
         $result = mysqli_query($this->conn, $sql);
         return $result;
     }
+}
 
-    public function insert(){
-        $sql = "";
+class RegistroAsistencia extends main{
+    public $conn;
+
+    public function get_all(){
+        $sql = "SELECT 
+            RegistrosAsistencia.id_empleado, 
+            Empleados.nombres, Empleados.apellidos, 
+            Empleados.email, Empleados.telefono, 
+            RegistrosAsistencia.fecha_hora, 
+            RegistrosAsistencia.dispositivo_info,
+            RegistrosAsistencia.estatus, 
+            RegistrosAsistencia.imagen_verificacion, 
+            Sedes.nombre, Sedes.latitud, Sedes.longitud 
+        FROM RegistrosAsistencia 
+        INNER JOIN Empleados ON Empleados.id_empleado = RegistrosAsistencia.id_empleado 
+        INNER JOIN Sedes ON Sedes.id_sede = RegistrosAsistencia.id_sede; ";
         $result = mysqli_query($this->conn, $sql);
         return $result;
     }
-
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
 require_once 'database/query.php';
-$r_asistencia = new Registro_Asistencia();
 
+$empleados = new Empleados();
 echo "<h1>Trabajadores</h1>";
 echo "<table border='1' style='width:100%; border-collapse: collapse; text-align: center;'>";
 echo "<tr><th>ID</th><th>Nombre</th><th>Imagen</th></tr>";
-
-foreach ($r_asistencia->get_all() as $row) {
+foreach ($empleados->get_all() as $row) {
     echo "<tr>";
     
     foreach ($row as $key => $item) {
@@ -22,6 +21,23 @@ foreach ($r_asistencia->get_all() as $row) {
 }
 echo "</table>";
 
+$asistencia = new RegistroAsistencia();
+echo "<h1>Lista de Asistencia</h1>";
+echo "<table border='1' style='width:100%; border-collapse: collapse; text-align: center;'>";
+echo "<tr><th>ID</th><th>Nombre</th><th>Apellidos</th><th>email</th><th>tel</th><th>fecha_hora</th></tr>";
+foreach ($asistencia->get_all() as $row) {
+    echo "<tr>";
+    
+    foreach ($row as $key => $item) {
+        if ($key == 'imagen_verificacion') {
+            echo "<td><img src='data:image/jpeg;base64," . base64_encode($item) . "' style='width: 50px; height: 50px;'/></td>";
+        } else {
+            echo "<td>" . htmlspecialchars($item) . "</td>";
+        }
+    }
+    echo "</tr>";
+}
+echo "</table>";
 
 
 ?>
