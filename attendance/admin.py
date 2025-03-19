291db13b5a2e33de3ed5ae473056bade803c7774
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-03-19 18:13:16 -0500

    feat: add base models

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from attendance import models
+
 # Register your models here.
+admin.site.register(models.Cargo)
+admin.site.register(models.Empleado)
+admin.site.register(models.Departamento)
--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Cargo(models.Model):
+    id_cargo = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+class Departamento(models.Model):
+    id_departamento = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+class Empleado(models.Model):
+    id_empleado = models.IntegerField(primary_key=True)
+    cedula = models.CharField(max_length=10)
+    nombres = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    fecha_contratacion = models.DateField()
+
+    id_departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    id_cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE)
+
+    # Estados 
+    ESTADOS = {
+        "ACT": "ACTIVO",
+        "INA": "INACTIVO",
+        "SUS": "SUSPENDIDO"
+    }
+    estado = models.CharField(max_length=3, choices=ESTADOS)
+
+    fecha_creacion = models.DateField(auto_now_add=True)
+    fecha_actualizacion = models.DateField(auto_now=True)
