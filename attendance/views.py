c9d70601892582d2b7fc4b6a5ef1672df105f8c1
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-03-20 09:12:13 -0500

    feat: add a template with owner view

--- attendance/views.py
+++ attendance/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+# Models
+from attendance.models import RegistroAsistencia
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    registros = RegistroAsistencia.objects.select_related('id_empleado', 'id_sede').values(
+        'id_empleado_id', 'id_empleado__nombres', 'id_empleado__apellidos',
+        'id_empleado__email', 'id_empleado__telefono',
+        'fecha_hora', 'dispositivo_info', 'estatus', 'imagen_verificacion',
+        'id_sede__nombre', 'latitud', 'longitud'
+    )
+    print(registros)
+    return render(request, 'home.html', {'registros': registros})
