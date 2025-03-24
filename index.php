e0a20c07b75b4eda434d2f9967f6c62a381cdeef
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-03-24 11:37:40 -0500

    feat: update index.html

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+/**
 require_once 'modules/face_compare.php';
 require_once 'api/handler.php';
 $rostros = new Rostros();
 $api = new ApiHandler($rostros);
 $response = $api->handleRequest();
 echo $response;
-
+ */
 ?>
+<!-- 
 <form action="http://127.0.0.1:82/" method="POST" enctype="multipart/form-data">
         <label for="file">Selecciona una foto:</label>
         <input type="file" id="file" name="file" accept="image/png" required>
@@ -25,17 +27,15 @@ echo $response;
 </form>
 
 <form action="http://127.0.0.1:82/" method="POST" enctype="multipart/form-data">
-    <!-- Campo para el archivo -->
     <label for="file">Selecciona una imagen:</label>
     <input type="file" name="file" id="file" required><br><br>
         
-    <!-- Campo para la cadena binaria -->
     <label for="binary">Introduce la cadena binaria:</label><br>
     <textarea name="binary" id="binary" rows="4" cols="50" required></textarea><br><br>
         
-    <!-- Botón de envío -->
     <input type="submit" value="Enviar">
  </form>
+ -->
 
 <?php
 
